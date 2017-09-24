1b5e511fbffc633b23bfe35a35631c37ada482fd
Jason Hsu <jhsu802701@jasonhsu.com>
2017-09-24 01:32:46 +0000

    Updated the applicationhelper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
   # BEGIN: full_title
   # Returns the full title on a per-page basis
   def full_title(page_title = '')
-    base_title = 'Generous IT'
+    base_title = 'generous IT'
     if page_title.empty?
       base_title
     else
